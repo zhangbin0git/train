eb35eb887d0c287e2f03dbdcc7402dd1f61c53d2
zhangbin <zhangbin.email@foxmail.com>
2019-04-24 18:07:18 +0800

    in office

--- web_post/README.rst
+++ web_post/README.rst
@@ -40,6 +40,15 @@
 
      (.venv)$ pip install -e .
 
+结构设计
+------------------
+# 前台开启浏览器模式
+# 用户登录授权操作
+# 从售电API端口提取数据
+# 上报数据
+# 效验上报情况，反馈
+# 对上报情况通过email通知用户
+
 变更依赖库时
 ---------------------
 
@@ -64,7 +73,52 @@
 3. 将setup.py和requirements.txt提交到版本库
 
 
+ # 点击数据上报按钮
+        driver.find_element_by_xpath("//div[contains(text(), '数据上报')]").click()
+        time.sleep(2)
+        # driver.find_element_by_xpath(
+        #     "//label[contains(text(), '晋能售电公司售电日报')]").click()
+        # 点击晋能售电公司售电日报按钮
+        mid = driver.find_element_by_xpath("//label[contains(text(), '售电日报')]")
+        # time.sleep(3)
+        mid.click()
+        time.sleep(3)
+        # 切换frame
+        driver.switch_to_frame("mainFrame")
+        # 点击新建按钮
+        mik = driver.find_element_by_id("New")
+        # time.sleep(3)
+        mik.click()
+        time.sleep(1)
+        # 填入数据
+        driver.find_element_by_id("tab0_text2").send_keys("1234")
+        driver.find_element_by_id("tab0_text3").send_keys("1234")
+        driver.find_element_by_id("tab0_text4").send_keys("1234")
+        driver.find_element_by_id("tab0_text5").send_keys("1234")
+        # 点击保存按钮
+        driver.find_element_by_id("Save").click()
+        time.sleep(1)
+        # 点击所有操作按钮
+        driver.find_element_by_id("taskDetail").click()
+        # 点击提交按钮
+        driver.find_element_by_class_name('wf_mouseout').click()
+        driver.close()
 
+        print('查询操作完毕！')
+
+
+        # def open_page(driver):
+        # driver.driver.find_element_by_id("tree_NodeLabel_P6180").click()
+
+    # def sub_post(driver, url):
+    #     driver.get(url)
+
+    # 方法主入口
+    if __name__ == '__main__':
+        # 加启动配置
+        driver = openChrome(path)
+        operationAuth(driver, url)
+        # open_page(driver)
 
 
 
--- web_post/selenium_chrome_post.py
+++ web_post/selenium_chrome_post.py
@@ -2,79 +2,49 @@
 # -*- coding: utf-8 -*- 
 # @Time : 2019-4-3 18:23 
 # @Author : Mark 
-# @Site :  
+# @Site : office
 # @File : selenium_chrome_post.py 
 # @Software: PyCharm Community Edition
+# 接口对接协议的密码和开发人有关
 
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
 import time
 url = r"http://10.1.3.33:7001/Liems/"
+username = r"JNDLLLSD"
+password = r"1234"
 path = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
 
-# 前台开启浏览器模式
-def openChrome(path):
-    # 打开chrome浏览器
-    driver = webdriver.Chrome(path)
-    return driver
+class WebPost():
+    """通过从售电公司api端口提取当日售电量数据，
+    自动登录晋能电力集团生产管理信息系统上报当日售电量"""
+    def __init__(self, path):
+        # 必要参数path：chrome插件地址
+        self.path = path
 
-# 授权操作
-def operationAuth(driver, url):
-    driver.get(url)
-    # 找到输入框并输入查询内容
-    luku_user = driver.find_element_by_id("userName")
-    luku_user.send_keys("JNDLLLSD")
-    luku_pwd = driver.find_element_by_id("password")
-    luku_pwd.send_keys("1234")
-    # 提交表单
-    # "menuIdrandom5 splitLine topButton"
-    driver.find_element_by_class_name("login_btn").click()
-    # 以上完成登录工作
+    def open_chrome(self, path):
+        # # 前台开启浏览器模式,返回为打开的浏览器实体，path：chrome插件地址
+        driver = webdriver.Chrome(path)
+        return driver
 
-    # 点击数据上报按钮
-    driver.find_element_by_xpath("//div[contains(text(), '数据上报')]").click()
-    time.sleep(2)
-    # driver.find_element_by_xpath(
-    #     "//label[contains(text(), '晋能售电公司售电日报')]").click()
-    # 点击晋能售电公司售电日报按钮
-    mid = driver.find_element_by_xpath("//label[contains(text(), '售电日报')]")
-    # time.sleep(3)
-    mid.click()
-    time.sleep(3)
-    # 切换frame
-    driver.switch_to_frame("mainFrame")
-    # 点击新建按钮
-    mik = driver.find_element_by_id("New")
-    # time.sleep(3)
-    mik.click()
-    time.sleep(1)
-    # 填入数据
-    driver.find_element_by_id("tab0_text2").send_keys("1234")
-    driver.find_element_by_id("tab0_text3").send_keys("1234")
-    driver.find_element_by_id("tab0_text4").send_keys("1234")
-    driver.find_element_by_id("tab0_text5").send_keys("1234")
-    # 点击保存按钮
-    driver.find_element_by_id("Save").click()
-    time.sleep(1)
-    # 点击所有操作按钮
-    driver.find_element_by_id("taskDetail").click()
-    # 点击提交按钮
-    driver.find_element_by_class_name('wf_mouseout').click()
-    driver.close()
+    def operation_auth(self, url, username, password):
+        """用户授权登录"""
+        # 打开指定的网址
+        self.driver = self.open_chrome(self.path)
+        self.driver.get(url)
+        # 找到输入框
+        sys_user = self.driver.find_element_by_id("userName")
+        # 输入指定内容
+        sys_user.send_keys(username)
+        sys_pwd = self.driver.find_element_by_id("password")
+        sys_pwd.send_keys(password)
+        # 提交表单，查找提交按钮并点击
+        self.driver.find_element_by_class_name("login_btn").click()
 
-    print('查询操作完毕！')
+    def close_windows(self):
+        """关闭窗口"""
+        self.driver.quit()
 
-
-# def open_page(driver):
-    # driver.driver.find_element_by_id("tree_NodeLabel_P6180").click()
-
-
-# def sub_post(driver, url):
-#     driver.get(url)
-
-# 方法主入口
-if __name__ == '__main__':
-    # 加启动配置
-    driver = openChrome(path)
-    operationAuth(driver, url)
-    # open_page(driver)
+web_post = WebPost(path)
+web_post.operation_auth(url, username, password)
+web_post.close_windows()
