ccf4d990f33e1fe676e56ab6982f665f20a73fd9
zhangbin <zhangbin.email@foxmail.com>
2019-04-25 12:01:06 +0800

    in office

--- web_post/README.rst
+++ web_post/README.rst
@@ -48,6 +48,8 @@
 # 上报数据
 # 效验上报情况，反馈
 # 对上报情况通过email通知用户
+# 关闭所有窗口
+# 整体封装运行
 
 变更依赖库时
 ---------------------
@@ -73,45 +75,7 @@
 3. 将setup.py和requirements.txt提交到版本库
 
 
- # 点击数据上报按钮
-        driver.find_element_by_xpath("//div[contains(text(), '数据上报')]").click()
-        time.sleep(2)
-        # driver.find_element_by_xpath(
-        #     "//label[contains(text(), '晋能售电公司售电日报')]").click()
-        # 点击晋能售电公司售电日报按钮
-        mid = driver.find_element_by_xpath("//label[contains(text(), '售电日报')]")
-        # time.sleep(3)
-        mid.click()
-        time.sleep(3)
-        # 切换frame
-        driver.switch_to_frame("mainFrame")
-        # 点击新建按钮
-        mik = driver.find_element_by_id("New")
-        # time.sleep(3)
-        mik.click()
-        time.sleep(1)
-        # 填入数据
-        driver.find_element_by_id("tab0_text2").send_keys("1234")
-        driver.find_element_by_id("tab0_text3").send_keys("1234")
-        driver.find_element_by_id("tab0_text4").send_keys("1234")
-        driver.find_element_by_id("tab0_text5").send_keys("1234")
-        # 点击保存按钮
-        driver.find_element_by_id("Save").click()
-        time.sleep(1)
-        # 点击所有操作按钮
-        driver.find_element_by_id("taskDetail").click()
-        # 点击提交按钮
-        driver.find_element_by_class_name('wf_mouseout').click()
-        driver.close()
 
-        print('查询操作完毕！')
-
-
-        # def open_page(driver):
-        # driver.driver.find_element_by_id("tree_NodeLabel_P6180").click()
-
-    # def sub_post(driver, url):
-    #     driver.get(url)
 
     # 方法主入口
     if __name__ == '__main__':
--- web_post/selenium_chrome_post.py
+++ web_post/selenium_chrome_post.py
@@ -6,11 +6,21 @@
 # @File : selenium_chrome_post.py 
 # @Software: PyCharm Community Edition
 # 接口对接协议的密码和开发人有关
-
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email import utils
+import smtplib
+import json
+import requests
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
+import datetime
 import time
+
+# 相关参数
 url = r"http://10.1.3.33:7001/Liems/"
+api_url = r"http://10.35.1.21/interface-platform-web/invoke/bloc_getElecPq.do"
 username = r"JNDLLLSD"
 password = r"1234"
 path = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
@@ -23,7 +33,8 @@ class WebPost():
         self.path = path
 
     def open_chrome(self, path):
-        # # 前台开启浏览器模式,返回为打开的浏览器实体，path：chrome插件地址
+        """前台开启浏览器模式"""
+        # 返回为打开的浏览器实体，path：chrome插件地址
         driver = webdriver.Chrome(path)
         return driver
 
@@ -41,12 +52,117 @@ class WebPost():
         # 提交表单，查找提交按钮并点击
         self.driver.find_element_by_class_name("login_btn").click()
 
+    def gen_para(self, startdate, enddate, clientKey="d1885d575a7211e988f500e066efe6e6", serviceKey="bloc_getElecPq"):
+        """生产客户端请求json，数据。包括起止时间"""
+        parameter = \
+            {"clientKey":clientKey,
+            "params":{"startDate":startdate, "endDate":enddate},
+            "serviceKey":serviceKey}
+        return parameter
+
+    def get_data(self, api_url):
+        """从售电API端口提取数据"""
+        # 获取昨天日期
+        yesterday = datetime.date.today() + datetime.timedelta(-1)
+        # 格式化成2018-01-01形式
+        startdate = yesterday.strftime("%Y-%m-%d")
+        enddate = yesterday.strftime("%Y-%m-%d")
+        # 生产请求参数
+        req = self.gen_para(startdate, enddate)
+        # 指定json.dumps生成的字符串之后,可以直接发送数据而不进行URL编码
+        # 需要明确指定Content-Tpye
+        response = requests.post(api_url, json.dumps(req),
+                                 headers={'Content-Type': 'application/json'})
+        return response.json()
+
+    def post_data(self, tab0_text2, tab0_text3, tab0_text4, tab0_text5):
+        """上报数据"""
+        # 点击"数据上报"按钮
+        self.driver.find_element_by_xpath("//div[contains(text(), "
+                                          "'数据上报')]").click()
+        # 等待页面加载
+        time.sleep(2)
+        # 点击"晋能售电公司售电日报"按钮
+        self.driver.find_element_by_xpath("//label[contains(text(), "
+                                           "'售电日报')]").click()
+        # 等待页面加载
+        time.sleep(2)
+        # 由于网站使用frame框架，切换frame
+        self.driver.switch_to_frame("mainFrame")
+        # 点击"新建"按钮
+        self.driver.find_element_by_id("New").click()
+        # 等待页面加载
+        time.sleep(2)
+        # 填入数据
+        self.driver.find_element_by_id("tab0_text2").send_keys(tab0_text2)
+        self.driver.find_element_by_id("tab0_text3").send_keys(tab0_text3)
+        self.driver.find_element_by_id("tab0_text4").send_keys(tab0_text4)
+        self.driver.find_element_by_id("tab0_text5").send_keys(tab0_text5)
+        # 点击"保存"按钮
+        self.driver.find_element_by_id("Save").click()
+        # 等待页面加载
+        time.sleep(2)
+        # 点击所有操作按钮
+        self.driver.find_element_by_id("taskDetail").click()
+        # 点击"提交"按钮
+        self.driver.find_element_by_class_name("wf_mouseout").click()
+        self.driver.close()
+        return "200"
+
+    def feedback(self):
+        """效验上报情况，反馈"""
+        pass
+
+    def send_email(self, from_addr, password, to_addr, smtp_server):
+        """对上报情况通过email通知用户"""
+        from_addr = from_addr
+        password = password
+        to_addr = to_addr
+        smtp_server = smtp_server
+        # 发送相关信息，应html的格式展示
+        msg = MIMEText('<html><body><h1>上报正常</h1>' +
+            '<p>send by administrator</p>' + '</body></html>', 'html', 'utf-8')
+        msg['From'] = self._format_addr(u'生产日报自动上报客户端 <%s>' % from_addr)
+        msg['To'] = self._format_addr(u'管理员 <%s>' % to_addr)
+        msg['Subject'] = Header(u'生产日报自动上报情况', 'utf-8').encode()
+        # 服务器配置
+        server = smtplib.SMTP(smtp_server, 25)
+        # 打印出和SMTP服务器交互的所有信息
+        server.set_debuglevel(1)
+        server.login(from_addr, password)
+        server.sendmail(from_addr, [to_addr], msg.as_string())
+        server.quit()
+
     def close_windows(self):
-        """关闭窗口"""
+        """关闭所有窗口"""
         self.driver.quit()
-    def send_email(self,user_email):
-        """将上报情况以email形式发送给用户"""
+
+    def _format_addr(self, s):
+        # 来格式化一个邮件地址
+        name, addr = utils.parseaddr(s)
+        return utils.formataddr(
+            (Header(name, 'utf-8').encode(),
+             addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+    def run(self):
+        """整体封装运行"""
+        # 打开界面，登录系统
+        self.operation_auth(url, username, password)
+        # 从售电API端口提取数据
+        # 上报数据
+        self.post_data(tab0_text2, tab0_text3, tab0_text4, tab0_text5)
+        # 效验上报情况，反馈
+        # 对上报情况通过email通知用户
+        # 关闭所有窗口
+        self.close_windows()
+        # 整体封装运行
+
+
+
+
 
 web_post = WebPost(path)
-web_post.operation_auth(url, username, password)
-web_post.close_windows()
+# web_post.operation_auth(url, username, password)
+# web_post.close_windows()
+response = web_post.get_data(api_url)
+print(response)
